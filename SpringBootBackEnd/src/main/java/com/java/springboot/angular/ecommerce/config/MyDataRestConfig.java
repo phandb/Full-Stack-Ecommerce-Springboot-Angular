e23cdd61a8900a3b77ad51c9bffb7f34d1d63d13
Dalton <phandb72@gmail.com>
2020-04-08 22:36:01 -0500

    Create data REST configuration implements RepositoryRestConfigurer

--- SpringBootBackEnd/src/main/java/com/java/springboot/angular/ecommerce/config/MyDataRestConfig.java
+++ SpringBootBackEnd/src/main/java/com/java/springboot/angular/ecommerce/config/MyDataRestConfig.java
@@ -1,10 +1,21 @@
 package com.java.springboot.angular.ecommerce.config;
 
-import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import com.java.springboot.angular.ecommerce.entity.Product;
+import com.java.springboot.angular.ecommerce.entity.ProductCategory;
+
+// import org.hibernate.type.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer{
@@ -14,6 +25,46 @@ public class MyDataRestConfig implements RepositoryRestConfigurer{
     @Autowired
     public MyDataRestConfig(EntityManager theEntityManager){
         entityManager = theEntityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        //  Read only--Disable Http methods for Product: PUT, POST, and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+               
+        //  Read only--Disable Http methods for ProductCategory: PUT, POST, and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+         
+        // Call an internal helper to expose id
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // expose entity ids
+
+        // Get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // Create an array of the entity types
+        List<Class<?>> entityClasses = new ArrayList<>();
+
+        // Get the entity types for the entities
+        for (EntityType<?> tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // Expose the entity ids for the arrays of entity/domain types
+        Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
 
