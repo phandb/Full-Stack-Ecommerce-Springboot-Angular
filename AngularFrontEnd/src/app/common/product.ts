d60ab8b6776db5ea7b0e34675d00fb9408369f07
Dalton <phandb72@gmail.com>
2020-04-10 20:53:42 -0500

    Create classes Product and ProductCategory

---
+++ AngularFrontEnd/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+    // Define properties that matched JSon properties return
+    id: string;
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+}
