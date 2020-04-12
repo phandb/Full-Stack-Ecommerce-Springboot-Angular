6b605c33e4c4525530516397d86261c69af619cd
Dalton <phandb72@gmail.com>
2020-04-12 09:11:57 -0500

    Generate search, product-detail, prduct-category-menu components

--- AngularFrontEnd/src/app/app.module.ts
+++ AngularFrontEnd/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule} from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent,
+    ProductDetailsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
