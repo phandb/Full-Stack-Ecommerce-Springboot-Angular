9c13eff620e70b9579937e343313b9cb6ba99f11
Dalton <phandb72@gmail.com>
2020-04-15 12:46:25 -0500

    Build Product Detail Component

--- AngularFrontEnd/src/app/components/product-details/product-details.component.ts
+++ AngularFrontEnd/src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product = new Product();  //  Need to instantiate to avoid racing issue
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
+  }
+  handleProductDetails() {
+    // get the id from param string and convert to a number
+    const theProductId: number = +this.route.snapshot.paramMap.get('id');
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    );
   }
 
 }
