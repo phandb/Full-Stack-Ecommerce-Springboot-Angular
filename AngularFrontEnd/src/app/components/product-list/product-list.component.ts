57dea191fef408083f436b79fcd66d3b73884661
Dalton <phandb72@gmail.com>
2020-04-18 08:28:59 -0500

    Updated new UI using SB Admin Template

--- AngularFrontEnd/src/app/components/product-list/product-list.component.ts
+++ AngularFrontEnd/src/app/components/product-list/product-list.component.ts
@@ -5,7 +5,8 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  // templateUrl: './product-list.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 
