dfc81c9b7f5a8e065b55d1faa9b8f16e7c0cdb26
Dalton <phandb72@gmail.com>
2020-05-17 17:04:03 -0500

    Added Checkout Component

--- AngularFrontEnd/src/app/app.module.ts
+++ AngularFrontEnd/src/app/app.module.ts
@@ -12,9 +12,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 //  Define Routes
 const routes: Routes = [
+  { path: 'checkout', component: CheckoutComponent},
   { path: 'cart-details', component: CartDetailsComponent},
   { path: 'products/:id', component: ProductDetailsComponent},
   { path: 'search/:keyword', component: ProductListComponent},
@@ -33,13 +36,15 @@ const routes: Routes = [
     ProductDetailsComponent,
     SearchComponent,
     CartStatusComponent,
-    CartDetailsComponent
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
