fb118d36cd1de60e038cb126374c3a27e882e1b9
Dalton <phandb72@gmail.com>
2020-08-02 23:17:26 -0500

    Develop JPA Entities: Country and State

---
+++ SpringBootBackEnd/src/main/java/com/java/springboot/angular/ecommerce/entity/Country.java
@@ -0,0 +1,37 @@
+package com.java.springboot.angular.ecommerce.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="code")
+    private String code;
+
+    @Column(name="name")
+    private String name;
+
+    //  Setup one-to-many relationship with states
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+
+}
