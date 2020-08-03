e99f4a85f487ca70204a03ee2ba72d3d44ad8568
Dalton <phandb72@gmail.com>
2020-08-03 08:13:09 -0500

    Create TypeScript Classes: Country and State.  Add method to the service

--- AngularFrontEnd/src/app/services/shopping-form.service.ts
+++ AngularFrontEnd/src/app/services/shopping-form.service.ts
@@ -1,12 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Country } from '../common/country';
+import { map } from 'rxjs/operators';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingFormService {
 
-  constructor() { }
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCountries(): Observable<Country[]> {
+
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl)
+                          .pipe(map(
+                            response => response._embedded.countries
+                            ));
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+
+    // search for url
+    const searchStateUrl = '${this.stateUrl}/search/findByCountryCode?code=${theCountryCode}';
+
+    return  this.httpClient.get<GetResponseStates>(searchStateUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
 
@@ -38,4 +64,16 @@ export class ShoppingFormService {
 
     return of(data);
   }
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  };
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
+  };
 }
