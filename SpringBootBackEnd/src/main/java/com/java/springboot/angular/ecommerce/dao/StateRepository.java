f079aa0d10e90661a89907463e3cb8bb08607029
Dalton <phandb72@gmail.com>
2020-08-02 23:32:40 -0500

    Create Spring Data Repositories

---
+++ SpringBootBackEnd/src/main/java/com/java/springboot/angular/ecommerce/dao/StateRepository.java
@@ -0,0 +1,17 @@
+package com.java.springboot.angular.ecommerce.dao;
+
+import java.util.List;
+
+import com.java.springboot.angular.ecommerce.entity.State;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface StateRepository extends JpaRepository<State, Integer>{
+    
+    List<State> findByCountryCode(@Param("code") String code);
+}
--- SpringBootBackEnd/src/main/java/com/java/springboot/angular/ecommerce/entity/Country.java
+++ SpringBootBackEnd/src/main/java/com/java/springboot/angular/ecommerce/entity/Country.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -32,6 +34,7 @@ public class Country {
 
     //  Setup one-to-many relationship with states
     @OneToMany(mappedBy = "country")
+    @JsonIgnore  // Will not load states in country api
     private List<State> states;
 
 }
